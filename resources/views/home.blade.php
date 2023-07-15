4baa8fa4b4f4bc86b98ae89f858acc9b9129250c
tharsila <tharsilapaula@gmail.com>
2023-07-14 21:45:17 -0300

    feat: breadcrumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,19 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0">
+                @yield('title')
+            </h1>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                @yield('breadcrumb')
+            </ol>
+        </div>
+    </div>
 @stop
 
 @section('content')
